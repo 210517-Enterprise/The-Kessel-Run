83cd75e05f8f17a01cdd9d6dff35d69d76d2c3c6
thomaspceci <84198823+thomaspceci@users.noreply.github.com>
2021-07-15 15:33:15 -0400

    migrated to AWS

--- src/test/java/com/revature/TheKesselRunApplicationTests.java
+++ src/test/java/com/revature/TheKesselRunApplicationTests.java
@@ -2,8 +2,10 @@ package com.revature;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations="classpath:/src/test/resources/test.properties")
 class TheKesselRunApplicationTests {
 
 	@Test
