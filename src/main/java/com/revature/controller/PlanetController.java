dd365f80a4d26bea45077de9b8d90f00a755dd4d
thomaspceci <84198823+thomaspceci@users.noreply.github.com>
2021-07-16 12:54:14 -0400

    fixed bug

--- src/main/java/com/revature/controller/PlanetController.java
+++ src/main/java/com/revature/controller/PlanetController.java
@@ -9,7 +9,6 @@ import com.revature.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
