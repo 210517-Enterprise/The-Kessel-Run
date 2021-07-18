918d389ce8eb169560e58b3e147a275a2e0bfa21
andregoulbourne <andre.goulbourne2@gmail.com>
2021-07-18 18:36:41 -0400

    Fixed the login page

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -12,13 +12,13 @@ function createForm() {
             <div class="form-group row">
                 <label for="inputUsername" class="col-sm-3 col-form-label">Username</label>
                 <div class="col-sm-9">
-                    <input type="text" class="form-control" id="inputUsername" placeholder="Username">
+                    <input required type="text" class="form-control" id="inputUsername" placeholder="Username">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="inputPassword" class="col-sm-3 col-form-label">Password</label>
                 <div class="col-sm-9">
-                    <input type="password" class="form-control" id="inputPassword" style="font-family: Century Gothic, CenturyGothic, AppleGothic, sans-serif" placeholder="Password">
+                    <input required type="password" class="form-control" id="inputPassword" style="font-family: Century Gothic, CenturyGothic, AppleGothic, sans-serif" placeholder="Password">
                 </div>
             </div>
             <div class="row">
@@ -40,33 +40,27 @@ function createForm() {
     document.getElementById("form-submit").addEventListener("click", login)
     function login() {
 
-    let psd;    
+    var psd;    
 
-    let data = {username : document.getElementById("inputUsername").value, pass : document.getElementById("inputPassword").value}
-    console.log("username"+data.username)
-    console.log("password"+data.password)
-    fetch(`http://localhost:8080/users/${data.username}`)
-        .then(res => {
-            res.json()
-        })
-        .then(data1 =>{
-            psd=data1.password;
-            console.log(data1)
+    let data1 = {username : document.getElementById("inputUsername").value, pass : document.getElementById("inputPassword").value}
+    console.log("username"+data1.username)
+    console.log("password"+data1.pass)
+    fetch(`http://localhost:8080/users/${data1.username}`)
+        .then(resp => resp.json())
+        .then(data =>{
+            psd=data.pass;
+            if(psd===data1.pass&&psd!=undefined){
+                console.log("Logging in.")
+                sessionStorage.setItem("isLoggedIn", true)
+                sessionStorage.setItem("username", document.getElementById("inputUsername").value)
+                location.href= "planet.html";
+            } else {
+                console.error("Invalid Login Credentials.")
+                alert("Invalid Credentials")
+            } 
+        })           
             
-        })
-
-        console.log("psd"+psd)
-
-        if(psd===data.password){
-            console.log("Logging in.")
-            sessionStorage.setItem("isLoggedIn", true)
-            sessionStorage.setItem("username", document.getElementById("inputUsername").value)
-            location.href= "planet.html";
-        } else {
-            console.error("Invalid Login Credentials.")
-        }     
-            
-}
+    }
 
     
 }
